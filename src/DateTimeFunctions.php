569e01197dfc1b25b79828de0d7f75d97e4753c9
Sean Johnson <seanjohnson08@gmail.com>
2025-12-05 20:03:58 -0500

    DAY(), DAYOFMONTH(), CURDATE(), CURRENT_DATE(), CURTIME(), CURRENT_TIME(), CURRENT_TIMESTAMP()

--- src/DateTimeFunctions.php
+++ src/DateTimeFunctions.php
@@ -6,6 +6,63 @@ use DateTime;
 
 class DateTimeFunctions {
     /**
+     * Extracts the day from a date or datetime string.
+     */
+    public static function DAY(?string $datetime): ?int {
+        if ($datetime === null) {
+            return null;
+        }
+
+        $parsed = new DateTime($datetime);
+
+        return (int) $parsed->format('d');
+    }
+
+    /**
+     * Extracts the day from a date or datetime string.
+     */
+    public static function DAYOFMONTH(?string $datetime): ?int {
+        return self::DAY($datetime);
+    }
+
+    /**
+     * Returns the current date as a string in 'Y-m-d' format.
+     */
+    public static function CURDATE(): string {
+        $now = new DateTime();
+        return $now->format('Y-m-d');
+    }
+
+    /**
+     * Returns the current date as a string in 'Y-m-d' format.
+     */
+    public static function CURRENT_DATE(): string {
+        return self::CURDATE();
+    }
+
+    /**
+     * Returns the current time as a string in 'H:i:s' format.
+     */
+    public static function CURTIME(): string {
+        $now = new DateTime();
+        return $now->format('H:i:s');
+    }
+
+    /**
+     * Returns the current time as a string in 'H:i:s' format.
+     */
+    public static function CURRENT_TIME(): string {
+        return self::CURTIME();
+    }
+    
+    /**
+     * Returns the current date and time as a string in 'Y-m-d H:i:s' format.
+     */
+    public static function CURRENT_TIMESTAMP(): string {
+        return self::NOW();
+    }
+
+    /**
      * Extracts the month from a date or datetime string.
      */
     public static function MONTH(?string $datetime): ?int {
--- tests/DateTimeFunctionsTest.php
+++ tests/DateTimeFunctionsTest.php
@@ -75,4 +75,102 @@ class DateTimeFunctionsTest extends TestCase {
         // Allow for up to 5 seconds of difference
         $this->assertLessThanOrEqual(5, abs($now->getTimestamp() - $fetched->getTimestamp()));
     }
+
+    public function testDayDateTime() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT DAY("2025-12-04 10:47:00")
+        SQL);
+
+        $this->assertEquals($result->fetchColumn(0), 4);
+    }
+
+    public function testDayDate() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT DAY("2025-12-04")
+        SQL);
+
+        $this->assertEquals($result->fetchColumn(0), 4);
+    }
+
+    public function testDayNull() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT DAY(NULL)
+        SQL);
+
+        $this->assertNull($result->fetchColumn(0));
+    }
+
+    public function testDayOfMonthDateTime() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT DAYOFMONTH("2025-12-04 10:47:00")
+        SQL);
+
+        $this->assertEquals($result->fetchColumn(0), 4);
+    }
+
+    public function testDayOfMonthDate() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT DAYOFMONTH("2025-12-04")
+        SQL);
+
+        $this->assertEquals($result->fetchColumn(0), 4);
+    }
+
+    public function testCurDate() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT CURDATE()
+        SQL);
+
+        $now = new DateTimeImmutable();
+        $fetched = new DateTimeImmutable($result->fetchColumn(0));
+
+        $this->assertEquals($now->format('Y-m-d'), $fetched->format('Y-m-d'));
+    }
+
+    public function testCurTime() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT CURTIME()
+        SQL);
+
+        $now = new DateTimeImmutable();
+        $fetched = new DateTimeImmutable($result->fetchColumn(0));
+
+        $this->assertEquals($now->format('H:i'), $fetched->format('H:i'));
+    }
+
+    public function testCurrentDate() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT CURRENT_DATE()
+        SQL);
+
+        $now = new DateTimeImmutable();
+        $fetched = new DateTimeImmutable($result->fetchColumn(0));
+
+        $this->assertEquals($now->format('Y-m-d'), $fetched->format('Y-m-d'));
+    }
+
+    public function testCurrentTime() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT CURRENT_TIME()
+        SQL);
+
+        $now = new DateTimeImmutable();
+        $fetched = new DateTimeImmutable($result->fetchColumn(0));
+
+        $this->assertEquals($now->format('H:i'), $fetched->format('H:i'));
+    }
+
+    public function testCurrentTimestamp() {
+        $result = $this->pdo->query(<<<SQL
+            SELECT CURRENT_TIMESTAMP()
+        SQL);
+
+        $now = new DateTimeImmutable();
+        $fetched = new DateTimeImmutable($result->fetchColumn(0));
+
+        // Allow for up to 5 seconds of difference
+        $this->assertLessThanOrEqual(5, abs($now->getTimestamp() - $fetched->getTimestamp()));
+    }
+
+    
 }
